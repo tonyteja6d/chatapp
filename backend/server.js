0750971023ead290df75c6bf0cc736143257444d
tonyteja6d <tonytry6s@gmail.com>
2025-07-27 16:42:24 +0530

    Final dep

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path"
 
 import authRoutes from  "./routes/auth.routes.js"
 import messageRoutes from  "./routes/message.routes.js"
@@ -13,6 +14,9 @@ import { app, server } from "./socket/socket.js";
  
 const PORT = process.env.PORT || 5000;
 
+
+const __dirname = path.resolve();
+
 dotenv.config();
 
 
@@ -24,7 +28,11 @@ app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
  
+app.use(express.static(path.join(__dirname,"frontend/dist")))
 
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 // app.get("/",(req,res)=>{
 //     res.send(`hello world ${PORT} `);
